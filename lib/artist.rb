25311c560bf29f8f2fb291230aa35791e03f337f
Dakota Wicker <dwicker1996@gmail.com>
2020-04-07 21:05:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,40 +1,39 @@
 class Artist
+attr_accessor :name
 
-    attr_accessor :name, :songs
-    @@all =[]
-  
+@@all = []
+
     def initialize(name)
-      @name = name
-      @songs = []
+        @name = name
+        @@all << self
+        @songs =[]
     end
-  
+
+    def self.all
+        @@all
+    end
+
     def add_song(song)
-      @songs << song
+        @songs << song
+    end
+
+    def songs
+        @songs
+    end
+
+    def self.find_or_create_by_name (artist_name)
+        found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      self.new(artist_name)
       
     end
-  
-    def save
-      @@all << self 
     end
-  
-    def self.all
-      @@all
-    end
-  
-    def self.find_or_create_by_name(artist_name)
-      found_artist = self.all.find {|artist| artist.name == artist_name}
-      if found_artist
-        found_artist
-      else
-        new_artist = self.new(artist_name)
-        new_artist.save
-        new_artist
-      end
-    end
-  
+
     def print_songs
-      @songs.each do |song|
-        puts song.name
-      end
+        @songs.each do |song|
+            puts song.name
+        end
     end
-  end
+end
